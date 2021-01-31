0e3a8b7f286b80a9e96a3205d1664667504e0fc1
Iuri Vidal <iurividal10@gmail.com>
2021-01-31 07:59:01 -0300

    versao 1

---
+++ PDFProtectNet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PDFProtectNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var option = "Novo";
+
+
+            Console.WriteLine("Bem vindo");
+
+            do
+            {
+
+                Console.WriteLine("Informe o caminho do pdf");
+                var arquvio = Console.ReadLine();
+
+                Console.WriteLine("Certo, agora me informe a senha");
+                var senha = Console.ReadLine();
+
+                SetPassword.PdfSetPw pw = new SetPassword.PdfSetPw();
+                pw.Set(arquvio, senha);
+
+                Console.WriteLine("Arquivo está protegido");
+                Console.WriteLine("Digite Novo para proteger um novo arquivo ou Sair para fizanar a sessão");
+                option = Console.ReadLine();
+
+            } while (option != "Sair");
+
+
+        }
+    }
+}
---
+++ SetPassword/PdfSetPw.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using iText.Kernel;
+using iText.Kernel.Pdf;
+
+namespace SetPassword
+{
+    public class PdfSetPw
+    {
+
+
+
+        public void Set(string arquivo, string senha)
+        {
+
+            var item = arquivo;
+
+
+            if (!File.Exists(item)) return;
+
+            try
+            {
+
+                EncryptionProperties encryptionProperties = new EncryptionProperties();
+
+                var senhabyte = Encoding.ASCII.GetBytes(senha);
+
+                encryptionProperties.SetStandardEncryption(senhabyte, senhabyte, EncryptionConstants.ALLOW_PRINTING, EncryptionConstants.ENCRYPTION_AES_128);
+
+                using (Stream input = new FileStream(item, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var info = new FileInfo(item);
+
+                    var nameFile = Path.GetFileName(arquivo);
+
+                    var pdfencrypted = info.Directory + "\\encrypted.pdf";
+
+                    using (Stream output = new FileStream(pdfencrypted, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        iText.Kernel.Pdf.PdfReader reader = new PdfReader(input);
+                        PdfEncryptor.Encrypt(reader, output, encryptionProperties);
+
+                    }
+
+                    //  File.Delete(item);
+                    // File.Move(pdfencrypted, item);
+                }
+            }
+            catch (iText.Kernel.PdfException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+
+}
+
+
+
